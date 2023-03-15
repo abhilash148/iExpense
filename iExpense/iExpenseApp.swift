c97fea47a4b6f67d8d2329774dd0f9e2694c3dea
abhilash148 <saiabhilash148@gmail.com>
2023-03-15 02:10:03 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sai Abhilash Gudavalli on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
